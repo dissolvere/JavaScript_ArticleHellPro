55f68585682f9a3a9ccf0f51d7c74186fe4c7f16
MightyNox <noxmati@interia.pl>
2018-10-22 20:48:34 +0200

    Database Connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var config = require('../Config/config')
+
 var router = express.Router();
+
+mongoose.connect(config.databaseURI, { useNewUrlParser: true });
+
+var databaseConnection = mongoose.connection;
+databaseConnection.on('error', function(){
+  throw new Error("Cannot connect to the database!");
+});
+databaseConnection.once('open', function() {
+  console.log("Connected to the database!");
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+/* GET login panel */
+router.get('/login_panel', function(req, res, next) {
+  res.render('Login/login_panel', { title: 'Login Panel' });
+});
+
+/* 404 Not Found Error */
+router.get('*', function(req, res, next) {
+  res.render('Error/404', { title: '404 Not Found' });
+});
+
 
 module.exports = router;
